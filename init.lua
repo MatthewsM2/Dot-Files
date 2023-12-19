5812367712aa3ef557853c8c8deb004cd89455bb
Matthew M <matthew17773@gmail.com>
2023-12-19 21:41:32 +0530

    update files rm fonts in kitty

--- init.lua
+++ init.lua
@@ -1,23 +1,23 @@
 vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins ={
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
- {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' }
- }
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	}
 }
 
 local opts = {}
